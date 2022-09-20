ee2ee5c43c46a3432ac37c2ebf2d8195265ebb3e
Alex Browne <stephenalexbrowne@gmail.com>
2022-09-20 15:54:21 -0700

    Minor style adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./www/**/*.html", "./ts/**/*.ts"],
   theme: {
+    fontFamily: {
+      sans: ["Nunito", "sans-serif"],
+      serif: ["serif"],
+    },
     extend: {},
   },
   plugins: [],
