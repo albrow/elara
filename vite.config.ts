f1030057623aa4d0170c829b0666e5879601ad23
Alex Browne <stephenalexbrowne@gmail.com>
2022-11-01 17:04:42 -0700

    Add workarounds to fix github pages urls

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import wasmPack from "vite-plugin-wasm-pack";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Workaround for hosting on GitHub Pages.
+  // See: https://dev.to/shashannkbawa/deploying-vite-app-to-github-pages-3ane
+  base: "/elara/",
   root: "web",
   plugins: [react(), wasmPack("./elara-lib")],
 });
--- web/main.tsx
+++ web/main.tsx
@@ -7,23 +7,36 @@ import init from "../elara-lib/pkg";
 (async function () {
   await init();
 
+  // Workaround for hosting on GitHub Pages.
+  // See: https://itnext.io/so-you-want-to-host-your-single-age-react-app-on-github-pages-a826ab01e48
+  let routerBaseName = "/";
+  try {
+    // @ts-ignore
+    routerBaseName = process.env.PUBLIC_URL;
+  } catch {}
+
   // Importing other components *after* init() means the Components themselves
   // can be synchrounous and not worry about waiting for Wasm to load.
   const Root = (await import("./routes/root")).default;
   const Level = (await import("./routes/level")).default;
 
-  const router = createHashRouter([
+  const router = createHashRouter(
+    [
+      {
+        path: "/",
+        element: <Root />,
+        children: [
+          {
+            path: "/level/:levelNumber",
+            element: <Level />,
+          },
+        ],
+      },
+    ],
     {
-      path: "/",
-      element: <Root />,
-      children: [
-        {
-          path: "/level/:levelNumber",
-          element: <Level />,
-        },
-      ],
-    },
-  ]);
+      basename: routerBaseName,
+    }
+  );
 
   ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
