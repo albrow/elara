7121729e238ed8dd341fc56b4c7ac7c1c6e5707a
Alex Browne <stephenalexbrowne@gmail.com>
2023-02-01 13:12:08 -0800

    Add videos to variables and function outputs pages

---
+++ web/components/journal/youtube_video.tsx
@@ -0,0 +1,25 @@
+import { Box } from "@chakra-ui/react";
+
+export interface YoutubeVideoProps {
+  embedId: string;
+}
+
+export default function YoutubeVideo(props: YoutubeVideoProps) {
+  return (
+    <Box mt="30px" mb="40px">
+      <iframe
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          borderColor: "gray",
+          borderWidth: "1px",
+        }}
+        width="852"
+        height="480"
+        src={`https://www.youtube.com/embed/${props.embedId}?rel=0`}
+        allowFullScreen
+        title="YouTube video player"
+      />
+    </Box>
+  );
+}
