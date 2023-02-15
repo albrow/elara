c6e3ed79c025fe21d1f6317f42ad6e0960b36b57
Alex Browne <stephenalexbrowne@gmail.com>
2023-02-14 17:46:15 -0800

    Fix bug when loading gifs

--- web/lib/tutorial_shorts.ts
+++ web/lib/tutorial_shorts.ts
@@ -1,3 +1,7 @@
+import howToRunCodeUrl from "../images/shorts/how_to_run_code.gif";
+import howToPauseAndStepUrl from "../images/shorts/how_to_pause_and_step.gif";
+import howToSeeErrorsUrl from "../images/shorts/how_to_see_errors.gif";
+
 export interface TutorialShort {
   title: string;
   text: string;
@@ -8,17 +12,17 @@ export const SHORTS: { [key: string]: TutorialShort } = {
   how_to_run_code: {
     title: "How to run code",
     text: "Click the run button to run your code. The code will cause the rover to move around and do things.",
-    imageUrl: "/images/shorts/how_to_run_code.gif",
+    imageUrl: howToRunCodeUrl,
   },
   how_to_pause_and_step: {
     title: "One step at a time",
     text: "Click the pause button to pause your code while it is running. Then you can use adjacent buttons to run the code one step at a time.",
-    imageUrl: "/images/shorts/how_to_pause_and_step.gif",
+    imageUrl: howToPauseAndStepUrl,
   },
   how_to_see_errors: {
     title: "Error messages",
     text: "Sometimes your code may have an error. Don't worry, even the best coders get errors sometimes! If you hover over the red dot, you can see more information about the error.",
-    imageUrl: "/images/shorts/how_to_see_errors.gif",
+    imageUrl: howToSeeErrorsUrl,
   },
 };
 
