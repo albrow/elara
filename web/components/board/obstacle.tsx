96e653c97a2e8ea01b6737f0b4a32157e87b97e7
Alex Browne <stephenalexbrowne@gmail.com>
2023-02-15 15:25:30 -0800

    Fix bug in Obstacle component by explicitly returning null

--- web/components/board/obstacle.tsx
+++ web/components/board/obstacle.tsx
@@ -8,25 +8,26 @@ interface ObstacleProps {
 }
 
 export default function Obstacle(props: ObstacleProps) {
-  return (
+  if (props.fuzzy) {
     // Might change this later, but for now, if the obstacle is "fuzzy",
     // we don't want to render it. "Fuzzy" means that the obstacle may
     // or may not be there. Eventually, we will probably use an animation.
     //
     // TODO(albrow): Add animation of obstacles falling/sliding into place.
-    !props.fuzzy && (
-      <img
-        className="rock sprite"
-        alt="rock"
-        src={rockImgUrl}
-        style={{
-          width: `${TILE_SIZE - 1}px`,
-          height: `${TILE_SIZE - 1}px`,
-          zIndex: WALL_Z_INDEX,
-          left: props.offset.left,
-          top: props.offset.top,
-        }}
-      />
-    )
+    return null;
+  }
+  return (
+    <img
+      className="rock sprite"
+      alt="rock"
+      src={rockImgUrl}
+      style={{
+        width: `${TILE_SIZE - 1}px`,
+        height: `${TILE_SIZE - 1}px`,
+        zIndex: WALL_Z_INDEX,
+        left: props.offset.left,
+        top: props.offset.top,
+      }}
+    />
   );
 }
