ac7c0cd1babcd10d648899450c45dd26c632b196
Alex Browne <stephenalexbrowne@gmail.com>
2023-03-02 14:03:16 -0800

    Remove some old journal pages

--- web/components/journal/journal_section.tsx
+++ web/components/journal/journal_section.tsx
@@ -7,12 +7,9 @@ import { getNextSceneFromRoute } from "../../lib/scenes";
 import Functions from "./sections/functions.mdx";
 import FunctionOutputs from "./sections/function_outputs.mdx";
 import Comments from "./sections/comments.mdx";
-import Expressions from "./sections/expressions.mdx";
 import WhileLoops from "./sections/while_loops.mdx";
 import Variables from "./sections/variables.mdx";
-import Literals from "./sections/literals.mdx";
 import Arrays from "./sections/arrays.mdx";
-import MathExpressions from "./sections/math_expressions.mdx";
 import Comparisons from "./sections/comparisons.mdx";
 import IfStatements from "./sections/if_statements.mdx";
 import Loops from "./sections/loops.mdx";
@@ -23,9 +20,6 @@ export const sections = {
   comments: Comments,
   functions: Functions,
   function_outputs: FunctionOutputs,
-  expressions: Expressions,
-  literals: Literals,
-  math_expressions: MathExpressions,
   loops: Loops,
   comparisons: Comparisons,
   variables: Variables,
