bc042a06fcb1bb702393323c4a8e204d9e13f872
Alex Browne <stephenalexbrowne@gmail.com>
2023-04-13 14:25:08 -0700

    Add indicators for possible astroid strikes

--- web/components/board/obstacle.tsx
+++ web/components/board/obstacle.tsx
@@ -1,3 +1,7 @@
+import { MdOutlineWarningAmber } from "react-icons/md";
+
+import { Box } from "@chakra-ui/react";
+import { AnimateKeyframes } from "react-simple-animate";
 import { Offset } from "../../lib/utils";
 import { TILE_SIZE, WALL_Z_INDEX } from "../../lib/constants";
 import rockImgUrl from "../../images/rock.png";
@@ -9,12 +13,36 @@ interface ObstacleProps {
 
 export default function Obstacle(props: ObstacleProps) {
   if (props.fuzzy) {
-    // Might change this later, but for now, if the obstacle is "fuzzy",
-    // we don't want to render it. "Fuzzy" means that the obstacle may
-    // or may not be there. Eventually, we will probably use an animation.
-    //
-    // TODO(albrow): Add animation of obstacles falling/sliding into place.
-    return null;
+    // "Fuzzy" in this context means that there may or may not be an
+    // astroid strike at this location. Use a flashing warning icon to
+    // indicate this.
+    return (
+      <Box
+        position="absolute"
+        left={props.offset.left}
+        top={props.offset.top}
+        w={`${TILE_SIZE - 1}px`}
+        h={`${TILE_SIZE - 1}px`}
+        zIndex={WALL_Z_INDEX}
+        pt="5px"
+      >
+        <AnimateKeyframes
+          play
+          duration={0.85}
+          direction="alternate"
+          iterationCount="infinite"
+          keyframes={["opacity: 0", "opacity: 1"]}
+        >
+          <MdOutlineWarningAmber
+            size="40px"
+            color="var(--chakra-colors-red-500)"
+            style={{
+              margin: "auto",
+            }}
+          />
+        </AnimateKeyframes>
+      </Box>
+    );
   }
   return (
     <img
