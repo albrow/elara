6ff2627acbf8107fe8ee49dabc98b02c46d8262c
Alex Browne <stephenalexbrowne@gmail.com>
2023-04-24 17:09:25 -0700

    Flesh out telepad level a bit more

--- elara-lib/src/levels/telepad_part_one.rs
+++ elara-lib/src/levels/telepad_part_one.rs
@@ -16,6 +16,26 @@ lazy_static! {
     ];
 }
 
+impl TelepadPartOne {
+    // Note: We make obstacles a method so we can re-use the same set of
+    // obstacles for each possible state.
+    fn obstacles(&self) -> Vec<Obstacle> {
+        return vec![
+            Obstacle::new(0, 3),
+            Obstacle::new(0, 5),
+            Obstacle::new(1, 3),
+            Obstacle::new(1, 5),
+            Obstacle::new(2, 3),
+            Obstacle::new(2, 5),
+            Obstacle::new(3, 3),
+            Obstacle::new(3, 5),
+            Obstacle::new(4, 3),
+            Obstacle::new(4, 4),
+            Obstacle::new(4, 5),
+        ];
+    }
+}
+
 impl Level for TelepadPartOne {
     fn name(&self) -> &'static str {
         // TODO(albrow): Change this name.
@@ -31,18 +51,62 @@ impl Level for TelepadPartOne {
         &TELEPAD_FUNCS
     }
     fn initial_code(&self) -> &'static str {
+        // TODO(albrow): Add initial code.
         r#"
 "#
     }
+
     fn initial_states(&self) -> Vec<State> {
-        let mut state = State::new();
-        state.player = Player::new(0, 4, 20, Orientation::Right);
-        state.goal = Some(Goal {
-            pos: Pos { x: 11, y: 4 },
-        });
-        state.obstacles = vec![];
-        state.telepads = vec![Telepad::new((3, 4), (8, 4), Orientation::Left)];
-        vec![state]
+        vec![
+            State {
+                player: Player::new(0, 4, 10, Orientation::Right),
+                fuel_spots: vec![],
+                goal: Some(Goal {
+                    pos: Pos { x: 11, y: 4 },
+                }),
+                enemies: vec![],
+                obstacles: self.obstacles().clone(),
+                password_gates: vec![],
+                data_terminals: vec![],
+                telepads: vec![Telepad::new((3, 4), (8, 4), Orientation::Up)],
+            },
+            State {
+                player: Player::new(0, 4, 10, Orientation::Right),
+                fuel_spots: vec![],
+                goal: Some(Goal {
+                    pos: Pos { x: 11, y: 4 },
+                }),
+                enemies: vec![],
+                obstacles: self.obstacles().clone(),
+                password_gates: vec![],
+                data_terminals: vec![],
+                telepads: vec![Telepad::new((3, 4), (8, 4), Orientation::Down)],
+            },
+            State {
+                player: Player::new(0, 4, 10, Orientation::Right),
+                fuel_spots: vec![],
+                goal: Some(Goal {
+                    pos: Pos { x: 11, y: 4 },
+                }),
+                enemies: vec![],
+                obstacles: self.obstacles().clone(),
+                password_gates: vec![],
+                data_terminals: vec![],
+                telepads: vec![Telepad::new((3, 4), (8, 4), Orientation::Left)],
+            },
+            State {
+                player: Player::new(0, 4, 10, Orientation::Right),
+                fuel_spots: vec![],
+                goal: Some(Goal {
+                    pos: Pos { x: 11, y: 4 },
+                }),
+                enemies: vec![],
+                obstacles: self.obstacles().clone(),
+                password_gates: vec![],
+                data_terminals: vec![],
+                telepads: vec![Telepad::new((3, 4), (8, 4), Orientation::Right)],
+            },
+        ]
     }
     fn actors(&self) -> Vec<Box<dyn Actor>> {
         vec![]
@@ -54,6 +118,8 @@ impl Level for TelepadPartOne {
 
 #[cfg(test)]
 mod tests {
+    // TODO(albrow): Add tests.
+
     // use super::*;
     // use crate::constants::ERR_OUT_OF_FUEL;
     // use crate::levels::Outcome;
