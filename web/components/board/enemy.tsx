22e3a4f383f02c4c64b825051689fc41f698a96f
Alex Browne <stephenalexbrowne@gmail.com>
2023-05-04 17:03:33 -0700

    Rotate the lightning effect to make it appear more random

--- web/components/board/enemy.tsx
+++ web/components/board/enemy.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { Box } from "@chakra-ui/react";
 import { Offset } from "../../lib/utils";
 import { ENEMY_Z_INDEX, TILE_SIZE } from "../../lib/constants";
@@ -32,6 +32,18 @@ export default function Enemy(props: EnemyProps) {
       ),
     [props.animData, props.animState, props.enableAnimations]
   );
+
+  const [effectRotation, setEffectRotation] = useState(0);
+  const lightningImg = useRef<HTMLImageElement>(null);
+  useEffect(() => {
+    const ticker = setInterval(() => {
+      setEffectRotation((prev) => (prev + 90) % 360);
+      if (lightningImg.current) {
+        lightningImg.current.style.transform = `rotate(${effectRotation}deg)`;
+      }
+    }, 1500);
+    return () => clearInterval(ticker);
+  });
 
   const getRobotImgUrl = useCallback(() => {
     switch (props.facing) {
@@ -73,25 +85,42 @@ export default function Enemy(props: EnemyProps) {
             alt="enemy"
             className="enemy sprite"
             src={getRobotImgUrl()}
+            // We use the same image as for G.R.O.V.E.R. but with a filter
+            // that makes the malfunctioning rover look darker and more red in hue.
+            // TODO(albrow): Replace this with a separate sprite.
             style={{
               filter:
-                "drop-shadow(-2px 2px 2px rgba(0, 0, 0, 0.3)) hue-rotate(180deg) brightness(80%) sepia(20%) contrast(120%) saturate(200%)",
+                "drop-shadow(-2px 2px 2px rgba(0, 0, 0, 0.3)) hue-rotate(180deg) " +
+                "brightness(80%) sepia(20%) contrast(120%) saturate(200%)",
             }}
           />
-          <img
-            src={lightningEffectUrl}
-            alt="lightning effect"
+          <Box
+            // If the rover is facing to the side, the perspective means that the body
+            // of the rover is shorter in the y dimension. To compensate, we scale the
+            // lightning effect down so it matches more closely with the rover body.
             style={{
-              position: "relative",
-              top: `${TILE_SIZE * 0.15}px`,
-              left: `${TILE_SIZE * 0.15}px`,
-              width: `${TILE_SIZE * 0.7}px`,
-              height: `${TILE_SIZE * 0.7}px`,
-              zIndex: ENEMY_Z_INDEX + 1,
-              filter:
-                "drop-shadow(0px 0px 4px rgba(255, 0, 0, 0.6)) saturate(200%) brightness(150%)",
+              transform:
+                props.facing === "left" || props.facing === "right"
+                  ? "scaleY(0.7)"
+                  : "none",
             }}
-          />
+          >
+            <img
+              ref={lightningImg}
+              src={lightningEffectUrl}
+              alt="lightning effect"
+              style={{
+                position: "relative",
+                top: `${TILE_SIZE * 0.2}px`,
+                left: `${TILE_SIZE * 0.2}px`,
+                width: `${TILE_SIZE * 0.6}px`,
+                height: `${TILE_SIZE * 0.6}px`,
+                zIndex: ENEMY_Z_INDEX + 1,
+                filter:
+                  "drop-shadow(0px 0px 4px rgba(255, 0, 0, 0.9)) saturate(200%) brightness(150%)",
+              }}
+            />
+          </Box>
         </div>
       </Box>
     </>
