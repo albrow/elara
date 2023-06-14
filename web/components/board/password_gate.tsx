0360615be7593ae09ef4c00b808b7019239487fb
Alex Browne <stephenalexbrowne@gmail.com>
2023-06-14 13:22:05 -0700

    Add speaker icon to differentiate gates and password gates

--- web/components/board/password_gate.tsx
+++ web/components/board/password_gate.tsx
@@ -2,6 +2,7 @@ import { Offset } from "../../lib/utils";
 import { GATE_Z_INDEX, TILE_SIZE } from "../../lib/constants";
 import lockedImgUrl from "../../images/locked.png";
 import unlockedImgUrl from "../../images/unlocked.png";
+import speakerImgUrl from "../../images/speaker.png";
 import BoardHoverInfo from "./board_hover_info";
 import PasswordGatePage from "./hover_info_pages/password_gate.mdx";
 
@@ -12,7 +13,6 @@ export interface PasswordGateProps {
   // fuzzy: boolean;
 }
 
-// TODO(albrow): Differentiate PasswordGate from Gate in the UI.
 export default function PasswordGate(props: PasswordGateProps) {
   return (
     <>
@@ -31,6 +31,19 @@ export default function PasswordGate(props: PasswordGateProps) {
           zIndex: GATE_Z_INDEX,
           left: props.offset.left,
           top: props.offset.top,
+          opacity: props.open ? 0.4 : 1,
+        }}
+      />
+      <img
+        alt="speaker"
+        src={speakerImgUrl}
+        style={{
+          position: "absolute",
+          width: `${TILE_SIZE / 2}px`,
+          height: `${TILE_SIZE / 2}px`,
+          zIndex: GATE_Z_INDEX,
+          left: props.offset.left,
+          top: `${props.offset.topNum + TILE_SIZE / 2}px`,
           opacity: props.open ? 0.4 : 1,
         }}
       />
