82e924bc38c54f39c4aaec786496871c2b877a3d
Alex Browne <stephenalexbrowne@gmail.com>
2023-06-14 13:31:45 -0700

    Update dialog for password gates

--- web/lib/dialog_trees/level_gates.ts
+++ web/lib/dialog_trees/level_gates.ts
@@ -1,15 +1,16 @@
 import { DialogChoice, DialogNode, DialogTrees } from ".";
 
-export type NodeIds = "explain_gates" | "provide_password";
+export type NodeIds = "explain_password_gates" | "provide_password";
 export type ChoiceIds = "request_password" | "ack_password";
 
 export const NODES: {
   [key in NodeIds]: DialogNode;
 } = {
-  explain_gates: {
+  explain_password_gates: {
     text:
-      `Looks like a locked gate is blocking the way! To open the gate, ` +
-      `move G.R.O.V.E.R. next to it, then say the password using the "say" function.`,
+      `Looks like a password gate is blocking the way! Instead of a button, this ` +
+      `kind of gate can be opened or closed by saying the password. First move ` +
+      `G.R.O.V.E.R. next to the gate, then say the password using the "say" function.`,
     choiceIds: ["request_password"],
   },
   provide_password: {
@@ -32,7 +33,7 @@ export const CHOICES: {
 
 export const TREES: DialogTrees = {
   level_gates: {
-    name: "Gates",
-    startId: "explain_gates",
+    name: "Password Gates",
+    startId: "explain_password_gates",
   },
 };
