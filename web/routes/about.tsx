05c4381a918f9b6a15f8692c63d200a7ac6e445c
Alex Browne <stephenalexbrowne@gmail.com>
2023-07-22 12:29:04 -0700

    Simplify about page

--- web/routes/about.tsx
+++ web/routes/about.tsx
@@ -1,12 +1,16 @@
-import { Box, Container } from "@chakra-ui/react";
+import { Box, Button, Container } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useRouteNode } from "react-router5";
 
+import { MdArrowBack } from "react-icons/md";
+
 import AboutContent from "../components/about.mdx";
 import "../styles/md_content.css";
+import { useSceneNavigator } from "../hooks/scenes_hooks";
 
 export default function About() {
   const { route } = useRouteNode("");
+  const { navigateToTitle } = useSceneNavigator();
 
   useEffect(() => {
     document.title = "Elara | About";
@@ -14,7 +18,13 @@ export default function About() {
 
   return (
     <Box w="100%" h="100%" position="fixed" bgColor="white">
-      <Container maxW="container.xl" p={8}>
+      <Box p="10px">
+        <Button colorScheme="blue" variant="ghost" onClick={navigateToTitle}>
+          <MdArrowBack size="1.3em" style={{ marginRight: "0.2rem" }} />
+          <span>Back to Title Screen</span>
+        </Button>
+      </Box>
+      <Container maxW="container.xl" px="30px" pb="30px">
         <div className="md-content">
           <AboutContent />
         </div>
