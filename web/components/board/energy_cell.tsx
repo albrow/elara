40e1bf356fbaf381fb8fd7d14154947a514b89d3
Alex Browne <stephenalexbrowne@gmail.com>
2023-08-01 11:37:40 -0700

    Add hovering animation for energy cells

--- web/components/board/energy_cell.tsx
+++ web/components/board/energy_cell.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@chakra-ui/react";
+import { AnimateKeyframes } from "react-simple-animate";
 import { Offset } from "../../lib/utils";
 import {
   TILE_SIZE,
@@ -10,14 +11,14 @@ import SpriteLabel from "./sprite_label";
 import EnergyCellPage from "./hover_info_pages/energy_cell.mdx";
 import BoardHoverInfo from "./board_hover_info";
 
-interface EnergyPodProps {
+interface EnergyCellProps {
   offset: Offset;
   energyGain?: number;
   collected: boolean;
   enableHoverInfo: boolean;
 }
 
-export default function EnergyPod(props: EnergyPodProps) {
+export default function EnergyCell(props: EnergyCellProps) {
   return props.collected ? null : (
     <>
       {props.enableHoverInfo && !props.collected && (
@@ -31,26 +32,41 @@ export default function EnergyPod(props: EnergyPodProps) {
         h={`${TILE_SIZE}px`}
         zIndex={ENERGY_CELL_Z_INDEX}
       >
-        <div
-          style={{
-            width: `${TILE_SIZE - 2}px`,
-            height: `${TILE_SIZE - 2}px`,
-            marginTop: "1px",
-            zIndex: ENERGY_CELL_Z_INDEX,
-            filter: "drop-shadow(-2px 3px 2px rgba(0, 0, 0, 0.3))",
-          }}
+        <Box
+          w={`${TILE_SIZE - 2}px`}
+          h={`${TILE_SIZE - 2}px`}
+          mt="1px"
+          zIndex={ENERGY_CELL_Z_INDEX}
         >
-          <img alt="energyCell" src={energyCellImgUrl} />
+          <AnimateKeyframes
+            keyframes={[
+              {
+                transform: "translateY(-3px)",
+                filter: "drop-shadow(0px 13px 2px rgba(0, 0, 0, 0.3))",
+              },
+              {
+                transform: "translateY(1px)",
+                filter: "drop-shadow(0px 9px 2px rgba(0, 0, 0, 0.3))",
+              },
+            ]}
+            play
+            iterationCount="infinite"
+            direction="alternate"
+            duration={1.5}
+            easeType="ease-in-out"
+          >
+            <img alt="energyCell" src={energyCellImgUrl} />
+          </AnimateKeyframes>
           <SpriteLabel
             zIndex={ENERGY_CELL_Z_INDEX + 1}
             value={`+${props.energyGain}`}
           />
-        </div>
+        </Box>
       </Box>
     </>
   );
 }
 
-EnergyPod.defaultProps = {
+EnergyCell.defaultProps = {
   energyGain: DEFAULT_ENERGY_CELL_GAIN,
 };
