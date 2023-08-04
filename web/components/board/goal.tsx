33ead41e0f25daeb2c917fe5f827fd44dc1a03ca
Alex Browne <stephenalexbrowne@gmail.com>
2023-08-04 11:19:10 -0700

    Add new pixel art for goal

--- web/components/board/goal.tsx
+++ web/components/board/goal.tsx
@@ -2,7 +2,7 @@ import { Image } from "@chakra-ui/react";
 
 import flagImgUrl from "../../images/board/flag.png";
 import { Offset } from "../../lib/utils";
-import { GOAL_Z_INDEX } from "../../lib/constants";
+import { GOAL_Z_INDEX, SPRITE_DROP_SHADOW } from "../../lib/constants";
 import GoalPage from "./hover_info_pages/goal.mdx";
 import BoardHoverInfo from "./board_hover_info";
 
@@ -26,6 +26,9 @@ export default function Goal(props: GoalProps) {
         zIndex={GOAL_Z_INDEX}
         left={props.offset.left}
         top={props.offset.top}
+        ml="1px"
+        mt="1px"
+        filter={SPRITE_DROP_SHADOW}
       />
     </>
   );
