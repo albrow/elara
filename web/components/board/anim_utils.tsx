052b6d506c58bf178dbc3fd1c426573135eb82e0
Alex Browne <stephenalexbrowne@gmail.com>
2023-08-25 19:59:16 -0700

    Remove old TODO

--- web/components/board/anim_utils.tsx
+++ web/components/board/anim_utils.tsx
@@ -138,7 +138,6 @@ export function getSpriteAnimations(
       definitions: getSpriteAnimationDefinitions(animState, animData, animId),
     };
   }
-  // TODO(albrow): Add shutting down animation here?
 
   return {
     style: {
