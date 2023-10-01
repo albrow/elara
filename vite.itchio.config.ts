4406a90972491c9b9ee4a824eb48c34666eb8181
Alex Browne <stephenalexbrowne@gmail.com>
2023-10-01 14:31:13 -0700

    Add build config for itch.io

---
+++ vite.itchio.config.ts
@@ -0,0 +1,24 @@
+// This config file includes some customizations for hosting on itch.io.
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import wasmPack from "vite-plugin-wasm-pack";
+
+export default defineConfig(async () => {
+  const mdx = await import("@mdx-js/rollup");
+  // @ts-ignore
+  const remarkGfm = await import("remark-gfm");
+  return {
+    // Set base="" for itch.io. This causes all assets to use relative paths,
+    // which is required. See https://itch.io/docs/creators/html5
+    base: "",
+    root: "web",
+    plugins: [
+      wasmPack("./elara-lib"),
+      react(),
+      mdx.default({ remarkPlugins: [remarkGfm.default as any] }),
+    ],
+    define: {
+      "import.meta.vitest": "undefined",
+    },
+  };
+});
