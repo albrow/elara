98a34d89ef8e71a3c41bc8e2ae5d2775d4683819
Alex Browne <stephenalexbrowne@gmail.com>
2023-11-20 17:03:19 -0800

    Open Electron app in fullscreen by default

--- electron/main.js
+++ electron/main.js
@@ -2,10 +2,14 @@ const path = require("node:path");
 const { app, BrowserWindow } = require("electron");
 
 const createWindow = () => {
+  // Create a new window and open it in fullscreen.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1080,
+    height: 700,
   });
+  win.maximize();
+  win.setMenuBarVisibility(false);
+  win.setFullScreen(true);
 
   win.loadFile(path.join(__dirname, "static", "index.html"));
 };
