1bf493d51e34d430192868228949196fb54e4629
Ashley Qian <ashley.qian.0@gmail.com>
2023-12-06 15:27:19 -0500

    chore: optimize lunar_surface_bg.png to *.jpg

--- web/components/board/board.tsx
+++ web/components/board/board.tsx
@@ -23,7 +23,7 @@ import {
 } from "../../lib/constants";
 import { Offset, posToOffset, range } from "../../lib/utils";
 import "./board.css";
-import lunarSurfaceBgUrl from "../../images/board/lunar_surface_bg.png";
+import lunarSurfaceBgUrl from "../../images/board/lunar_surface_bg.jpg";
 import DataPoint from "./data_point";
 import Enemy from "./enemy";
 import EnergyCell from "./energy_cell";
