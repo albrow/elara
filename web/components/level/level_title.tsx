6fa5d29614f54decbf27fe951a44b0c65faf061d
Ashley Qian <ashley.qian.0@gmail.com>
2023-12-09 15:12:50 -0500

    feat: responsive design for level page

--- web/components/level/level_title.tsx
+++ web/components/level/level_title.tsx
@@ -1,7 +1,9 @@
 import { Text, Tooltip, Box } from "@chakra-ui/react";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { RxDropdownMenu } from "react-icons/rx";
 import { useLevelSelectModal } from "../../hooks/level_select_modal_hooks";
+import { useWindowWidth } from "../../hooks/responsive_hooks";
+import { BP_2XL, BP_3XL, BP_XL, LEVEL_TITLE_FONT_SIZE_2XL, LEVEL_TITLE_FONT_SIZE_3XL, LEVEL_TITLE_FONT_SIZE_BASE, LEVEL_TITLE_FONT_SIZE_XL } from "../../lib/constants";
 
 export interface LevelTitleProps {
   title: string;
@@ -11,6 +13,22 @@ export interface LevelTitleProps {
 export default function LevelTitle(props: LevelTitleProps) {
   const [hoveringOver, setHoveringOver] = useState<boolean>(false);
   const [showLevelSelectModal] = useLevelSelectModal();
+  // Note: We have to use JavaScript to control the size of the dropdown menu icon because
+  // React Icons requires you to pass in the size as a string.
+  const windowWidth = useWindowWidth();
+
+  const dropdownMenuSize = useMemo(() => {
+    if (windowWidth >= BP_3XL) {
+      return LEVEL_TITLE_FONT_SIZE_3XL;
+    }
+    if (windowWidth >= BP_2XL) {
+      return LEVEL_TITLE_FONT_SIZE_2XL;
+    }
+    if (windowWidth >= BP_XL) {
+      return LEVEL_TITLE_FONT_SIZE_XL;
+    }
+    return LEVEL_TITLE_FONT_SIZE_BASE;
+  }, [windowWidth]);
 
   return (
     <Tooltip label="Select a different level" placement="right">
@@ -27,7 +45,7 @@ export default function LevelTitle(props: LevelTitleProps) {
         onClick={() => showLevelSelectModal()}
       >
         <RxDropdownMenu
-          size="1.4em"
+          size={dropdownMenuSize}
           style={{
             display: "inline-block",
             verticalAlign: "middle",
@@ -35,7 +53,12 @@ export default function LevelTitle(props: LevelTitleProps) {
           }}
         />
         <Text
-          fontSize="1.4em"
+          fontSize={{
+            base: LEVEL_TITLE_FONT_SIZE_BASE,
+            xl: LEVEL_TITLE_FONT_SIZE_XL,
+            "2xl": LEVEL_TITLE_FONT_SIZE_2XL,
+            "3xl": LEVEL_TITLE_FONT_SIZE_3XL,
+          }}
           fontWeight="bold"
           as="span"
           verticalAlign="middle"
