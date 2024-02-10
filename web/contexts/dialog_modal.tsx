97e6f59e7fd9c1f32ed8c1bddf8caf6630e66c04
Alex Browne <stephenalexbrowne@gmail.com>
2024-02-10 15:56:44 -0800

    Fix a bug where dialog modals stayed open if you went back to the hub

--- web/contexts/dialog_modal.tsx
+++ web/contexts/dialog_modal.tsx
@@ -9,9 +9,13 @@ import {
   createContext,
   PropsWithChildren,
   useCallback,
+  useEffect,
   useMemo,
   useState,
 } from "react";
+import { useRouter } from "react-router5";
+import { Unsubscribe } from "router5/dist/types/base";
+
 import DialogTree from "../components/dialog/dialog_tree";
 import { useSaveData } from "../hooks/save_data_hooks";
 
@@ -38,6 +42,7 @@ export function DialogModalProvider(props: PropsWithChildren<{}>) {
   const [visible, setVisible] = useState<boolean>(false);
   const [dialogTreeName, setDialogTreeName] = useState<string | null>(null);
   const [_, { markDialogSeen }] = useSaveData();
+  const router = useRouter();
 
   const showDialogModal = useCallback((treeName: string) => {
     setDialogTreeName(treeName);
@@ -58,6 +63,15 @@ export function DialogModalProvider(props: PropsWithChildren<{}>) {
     markDialogSeen(dialogTreeName!);
     setVisible(false);
   }, [dialogTreeName, markDialogSeen]);
+
+  // Always close the modal when the route changes.
+  useEffect(() => {
+    const unsubscribe = router.subscribe((_transition) => {
+      // Note: We don't mark the dialog as seen in this case.
+      setVisible(false);
+    }) as Unsubscribe;
+    return unsubscribe;
+  }, [router]);
 
   return (
     <DialogModalContext.Provider value={providerValue}>
