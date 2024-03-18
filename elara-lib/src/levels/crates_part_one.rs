a569cab78ec83a4b7f49bbf67b15b3b857bf3d7d
Alex Browne <stephenalexbrowne@gmail.com>
2024-03-17 17:06:02 -0700

    Add tests for crates_part_one

--- elara-lib/src/levels/crates_part_one.rs
+++ elara-lib/src/levels/crates_part_one.rs
@@ -46,99 +46,44 @@ impl Level for CratesPartOne {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use crate::constants::ERR_OUT_OF_ENERGY;
-//     use crate::levels::Outcome;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::levels::Outcome;
 
-//     #[test]
-//     fn level() {
-//         let mut game = crate::Game::new();
-//         const LEVEL: &'static dyn Level = &CratesPartOne {};
+    #[test]
+    fn level() {
+        let mut game = crate::Game::new();
+        const LEVEL: &'static dyn Level = &CratesPartOne {};
 
-//         // Running the initial code should result in Outcome::Continue.
-//         let script = LEVEL.initial_code();
-//         let result = game
-//             .run_player_script_with_all_funcs_unlocked(LEVEL, script.to_string())
-//             .unwrap();
-//         assert_eq!(result.outcome, Outcome::Continue);
+        // Running the initial code should result in Outcome::Continue.
+        let script = LEVEL.initial_code();
+        let result = game
+            .run_player_script_with_all_funcs_unlocked(LEVEL, script.to_string())
+            .unwrap();
+        assert_eq!(result.outcome, Outcome::Continue);
 
-//         // Running this code should result in Outcome::Success.
-//         let script = r#"move_forward(2); say("lovelace"); move_forward(5);"#;
-//         let result = game
-//             .run_player_script_with_all_funcs_unlocked(LEVEL, script.to_string())
-//             .unwrap();
-//         assert_eq!(result.outcome, Outcome::Success);
+        // Running this code should result in Outcome::Success.
+        let script = r#"
+            move_forward(1);
+            pick_up();
+            move_forward(3);"#;
+        let result = game
+            .run_player_script_with_all_funcs_unlocked(LEVEL, script.to_string())
+            .unwrap();
+        assert_eq!(result.outcome, Outcome::Success);
 
-//         // Attempting to reach the goal without saying the password should
-//         // result in running out of energy.
-//         let script = "loop { move_forward(1); }";
-//         let result = game
-//             .run_player_script_with_all_funcs_unlocked(LEVEL, script.to_string())
-//             .unwrap();
-//         assert_eq!(
-//             result.outcome,
-//             Outcome::Failure(ERR_OUT_OF_ENERGY.to_string())
-//         );
-
-//         // Saying the wrong password should not open the gate.
-//         let script = r#"move_forward(2); say("wrong password"); move_forward(5);"#;
-//         let result = game
-//             .run_player_script_with_all_funcs_unlocked(LEVEL, script.to_string())
-//             .unwrap();
-//         assert_eq!(result.outcome, Outcome::Continue);
-
-//         // Saying the password when not next to the gate should not
-//         // open it.
-//         let script = r#"say("lovelace"); move_forward(2); move_forward(5);"#;
-//         let result = game
-//             .run_player_script_with_all_funcs_unlocked(LEVEL, script.to_string())
-//             .unwrap();
-//         assert_eq!(result.outcome, Outcome::Continue);
-
-//         // Saying the password again, should close the gate, meaning we
-//         // can't reach the goal.
-//         let script = r#"move_forward(2); say("lovelace"); say("lovelace"); move_forward(5);"#;
-//         let result = game
-//             .run_player_script_with_all_funcs_unlocked(LEVEL, script.to_string())
-//             .unwrap();
-//         assert_eq!(result.outcome, Outcome::Continue);
-
-//         // Saying the password 3x, should result in the gate being open again.
-//         let script = r#"move_forward(2); say("lovelace"); say("lovelace"); say("lovelace"); move_forward(5);"#;
-//         let result = game
-//             .run_player_script_with_all_funcs_unlocked(LEVEL, script.to_string())
-//             .unwrap();
-//         assert_eq!(result.outcome, Outcome::Success);
-//     }
-
-//     #[test]
-//     fn challenge() {
-//         let mut game = crate::Game::new();
-//         const LEVEL: &'static dyn Level = &CratesPartOne {};
-
-//         // This code beats the level, but doesn't satisfy the challenge conditions.
-//         let script = r#"
-//             move_forward(2);
-//             say("lovelace");
-//             move_forward(5);"#;
-//         let result = game
-//             .run_player_script_with_all_funcs_unlocked(LEVEL, script.to_string())
-//             .unwrap();
-//         assert_eq!(result.outcome, Outcome::Success);
-//         assert!(!result.passes_challenge);
-
-//         // This code satisfies the challenge conditions.
-//         let script = r#"
-//             loop {
-//                 move_forward(2);
-//                 say("lovelace");
-//             }"#;
-//         let result = game
-//             .run_player_script_with_all_funcs_unlocked(LEVEL, script.to_string())
-//             .unwrap();
-//         assert_eq!(result.outcome, Outcome::Success);
-//         assert!(result.passes_challenge);
-//     }
-// }
+        // This should work too.
+        let script = r#"
+            move_forward(1);
+            pick_up();
+            turn_right();
+            drop();
+            turn_left();
+            move_forward(3);"#;
+        let result = game
+            .run_player_script_with_all_funcs_unlocked(LEVEL, script.to_string())
+            .unwrap();
+        assert_eq!(result.outcome, Outcome::Success);
+    }
+}
